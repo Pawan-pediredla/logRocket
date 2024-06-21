c1894e29e0d1d96fe2ddd20c41c7a2d395a16368
isafck <insaneesz1e@gmail.com>
2024-06-21 23:50:11 +0530

    adding extra files

--- resNavbar/navigation-bar/src/App.js
+++ resNavbar/navigation-bar/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './comp/Navbar';
 import Home from './comp/Home';
+import About from './comp/About';
 import News from './comp/News';
+import Favorite from './comp/Favorite';
+import GetStarted from './comp/GetStarted';
 function App() {
   return (
     <Router>
@@ -13,6 +16,11 @@ function App() {
   <Routes>
   <Route path='/' element={<Home />}  />
   <Route path='/news' element={<News/>}  />
+  <Route path='/about-us' element={<About/>}  />
+  <Route path='/favorite' element={<Favorite/>}  />
+  <Route path='/location' element={<location/>}  />
+  <Route path='/news' element={<GetStarted/>}  />  
+  
   </Routes>
 
   
